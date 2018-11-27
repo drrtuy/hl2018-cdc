892154fdb1895b00a071c29b8a98d87e299dbb75
drrtuy <drrtuy@gmail.com>
2018-11-27 21:27:16 +0300

    A new entrypoint for DA container is set. Shells has a reduced output. Changed IP addres into hostname in the XML. Make an empty config dir for Connect.

--- pg.add_connector.sh
+++ pg.add_connector.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data "@$1" | python -m json.tool
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data "@$1" &> /dev/null
--- pg.del_connector.sh
+++ pg.del_connector.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -i -X DELETE localhost:8083/connectors/$1 | python -m json.tool
+curl -i -X DELETE localhost:8083/connectors/$1 &> /dev/null | python -m json.tool
